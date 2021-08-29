98ab3570d09518244de088581b60dc3d9d2101a3
ziro <palembani@gmail.com>
2021-08-29 18:01:47 +0700

    + Added Slash class

--- core/objects.py
+++ core/objects.py
@@ -1,13 +1,33 @@
 """Collection of Object."""
-
+from __future__ import annotations
 
 import sqlite3
+from typing import Any
+
+import discord
 
 
 class Connection(sqlite3.Connection):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.execute("pragma journal_mode=wal")
         self.execute("pragma foreign_keys=ON")
         self.isolation_level = None
         self.row_factory = sqlite3.Row
+
+
+class Slash:
+    """Class for slash commands.
+
+    Planned usage:
+
+    class Hello(Slash, name="ping"):
+        async def callback(self, interaction):
+            await interaction.response.send_message("Hello World!")
+    """
+
+    def __init__(self, **kwargs) -> None:
+        pass
+
+    async def callback(self, interaction: discord.Interaction) -> Any:
+        raise NotImplementedError
