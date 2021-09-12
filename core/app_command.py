8c6c2116712b720eb28dd2eb696942ef235dfe4e
ziro <palembani@gmail.com>
2021-09-12 07:51:12 +0700

    + Added Option

--- core/app_command.py
+++ core/app_command.py
@@ -2,15 +2,45 @@
 from __future__ import annotations
 
 import asyncio
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional
 
 import discord
+from discord.utils import MISSING
 
 
 class Option:
-    """Class for 'chat input' application commands' option"""
+    """Class for 'CHAT-INPUT' application commands' option"""
 
-    pass
+    def __init__(
+        self,
+        name: str,
+        /,
+        *,
+        type: Any,
+        description: Optional[str] = None,
+        default: Any = MISSING,
+    ) -> None:
+        self.name: str = name
+        self.type: Any = type
+        self.description: Optional[str] = description
+        self.default: Any = default
+
+    @property
+    def isRequired(self) -> bool:
+        return self.default is MISSING
+
+    def toDict(self) -> Dict[str, Any]:
+        """Convert Slash object into dict.
+
+        Useful when registering Slash to discord
+        """
+        return {
+            "name": self.name,
+            # TODO: Make a converter, to convert python type to discord app cmd type
+            "type": 3,
+            "description": self.description or "No description",
+            "required": self.isRequired,
+        }
 
 
 class ApplicationCommand:
@@ -43,13 +73,19 @@ class Slash(ApplicationCommand):
 
     Planned usage:
 
+    # only for no argument and no subcommand slash
     @slash(...)
     async def hello(self, interaction):
         await interaction.response.send_message("Hello World!")
 
     # or
 
-    class Test(Slash):
+    class Test(Slash, name="test"):
+        def __init__(self, **kwargs):
+            super().__init__()
+            options = [Option("channel", type=discord.Channel)]
+            self.extendOptions(options)
+
         def callback(self, interaction):
             await interaction.response.send_message("テスト")
     """
@@ -62,6 +98,20 @@ class Slash(ApplicationCommand):
         self.description: Optional[str] = kwargs.get(
             "description", self.__app_description__
         )
+        self._options: List[Option] = []
+
+    @property
+    def options(self):
+        return self._options
+
+    def extendOptions(self, options: List[Option]) -> None:
+        self._options.extend(options)
+        return
+
+    def toDict(self) -> Dict[str, Any]:
+        ret = super().toDict()
+        ret["options"] = [opt.toDict() for opt in self.options]
+        return ret
 
     async def callback(self, interaction: discord.Interaction) -> Any:
         pass
@@ -87,6 +137,10 @@ async def hello(interaction: discord.Interaction):
 
 
 class Test(Slash):
+    def __init__(self):
+        super().__init__()
+        self.extendOptions([Option("name", type=str)])
+
     async def callback(self, interaction: discord.Interaction):
         await interaction.response.defer()
         await asyncio.sleep(5)
